e7c14c35e0a2853857968e685d8576c1f0506d1e
fahmy_sub <fahmyfye@gmail.com>
2018-04-25 15:34:11 +0800

    configure grunt file

--- dev/tools/grunt/configs/themes.js
+++ dev/tools/grunt/configs/themes.js
@@ -50,5 +50,15 @@ module.exports = {
             'css/styles'
         ],
         dsl: 'less'
+    },
+    fye: {
+        area: 'frontend',
+        name: 'Fye/customtheme',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
     }
 };
